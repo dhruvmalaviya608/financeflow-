39172b608612cc177e60ae397736df5da638a8d3
Mayur Malaviya <mayurmalaviya54@gmail.com>
2025-06-29 05:07:46 +0000

    ok i changed the logo name (logo.png)

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -158,7 +158,7 @@ export default function AppSidebar() {
     <div className="flex h-full max-h-screen flex-col gap-2 bg-card border-r">
       <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
         <Link href="/" className="flex items-center gap-2 font-semibold">
-          <img src="/Logo.png" alt="FinanceFlow Logo" className="h-8 w-8" />
+          <img src="/logo.png" alt="FinanceFlow Logo" className="h-8 w-8" />
           <span className="">FinanceFlow</span>
         </Link>
       </div>
